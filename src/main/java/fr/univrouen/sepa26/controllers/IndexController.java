c5ad692738fd332747d29949e33ef3a90b20c4aa
amouchas <ahcene.amouchas@univ-rouen.fr>
2026-04-07 14:03:22 +0200

    page home + help

--- src/main/java/fr/univrouen/sepa26/controllers/IndexController.java
+++ src/main/java/fr/univrouen/sepa26/controllers/IndexController.java
@@ -1,13 +1,21 @@
 package fr.univrouen.sepa26.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller //ici ce nest pas @RestController car on gere uniquement la vue aucune donnee
 public class IndexController {
 	@GetMapping("/")
+	// Le mot-clé "forward:" dit à Spring d'aller chercher le fichier
+	// dans src/main/resources/static/
 	public String index() {
-		return "Hello sepa26 automatisé!";
+		return "forward:/index.html";
+	}
+
+	@GetMapping("/help")
+	public String help() {
+		return "forward:/help.html";
 	}
 
 	@GetMapping("/simulate-error")
